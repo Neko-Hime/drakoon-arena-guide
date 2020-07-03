2ef34a92a754287a429df3023417e98b5ddd486a
Michał Walewski <mk.walewski@gmail.com>
2020-07-03 19:43:46 +0200

    1.0.3 - add some spawning circles

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+const ITEM_EGG = 912;
+const ITEM_CYLINDER = 553;
+
 module.exports = {
     EVENT_MESSAGE_TYPE: 40,
     DEFAULT_TEXT_COLOR: 'FFFF00',
@@ -16,15 +19,21 @@ module.exports = {
                     {id: 105, msg: 'Uppercut into Stun ([color="red"]stun inc[/color]) [B,I]', spawns: [], timeout: 0},
                     {id: 107, msg: 'Spectral Throw ([color="purple"]bleed[/color])', spawns: [], timeout: 0},
                     {id: 109, msg: 'Stomp into Spin ([color="red"]kd[/color]+[color="purple"]bleed[/color]) [B,I]', spawns: [], timeout: 0},
-                    {id: 110, msg: 'Quantum Wave', spawns: [], timeout: 0},
+                    {id: 110, msg: 'Quantum Wave', spawns: [
+                        {type: 'circle', pointDegrees: 0, pointRadius: 0, item: ITEM_EGG, time: 4000, intervalDegrees: 8, radius: 640, timeout: 0}
+                    ], timeout: 0},
                     {id: 111, msg: 'Leap ([color="red"]stun[/color]) [I]', spawns: [], timeout: 0},
                     {id: 112, msg: 'Kick into Backwave ([color="red"]push-back[/color]) [B,I]', spawns: [], timeout: 0},
                     {id: 113, msg: 'Double Upslash into Spin', spawns: [], timeout: 0},
                     {id: 115, msg: 'AoE Bombs', spawns: [], timeout: 0},
                     {id: 118, msg: 'Double Upslash into Front Stab', spawns: [], timeout: 0},
                     {id: 120, msg: 'Traverse Cut ([color="red"]stun[/color]) [B,I]', spawns: [], timeout: 0},
-                    {id: 121, msg: 'Purple Magic ([color="red"]Right Foot[/color], Inner donut first)', spawns: [], timeout: 0},
-                    {id: 124, msg: 'Purple Magic ([color="red"]Left Foot[/color], Outer donut first)', spawns: [], timeout: 0},
+                    {id: 121, msg: 'Purple Magic ([color="red"]Right Foot[/color], Inner donut first)', spawns: [
+                        {type: 'circle', pointDegrees: 0, pointRadius: 0, item: ITEM_CYLINDER, time: 3000, intervalDegrees: 8, radius: 540, timeout: 0}
+                    ], timeout: 0},
+                    {id: 124, msg: 'Purple Magic ([color="red"]Left Foot[/color], Outer donut first)', spawns: [
+                        {type: 'circle', pointDegrees: 0, pointRadius: 0, item: ITEM_CYLINDER, time: 3000, intervalDegrees: 8, radius: 540, timeout: 0}
+                    ], timeout: 0},
                     {id: 302, msg: 'Righteous Fire', spawns: [], timeout: 0},
                     {id: 304, msg: 'Shield ([color="purple"]plague[/color]/[color="blue"]regression[/color])', spawns: [], timeout: 0},
                 ],
